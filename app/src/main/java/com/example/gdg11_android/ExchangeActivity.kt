635c54641bac1496f2f5cfa445f40c9d578a1d5e
junieberry <junyoung44@g.skku.edu>
2021-07-10 01:16:53 +0900

    [feat] exchange activity basic design

--- app/src/main/java/com/example/gdg11_android/ExchangeActivity.kt
+++ app/src/main/java/com/example/gdg11_android/ExchangeActivity.kt
@@ -1,15 +1,11 @@
 package com.example.gdg11_android
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ExchangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        var ex= Intent(this, ExchangeActivity::class.java)
-        startActivity(ex)
+        setContentView(R.layout.activity_exchange)
     }
 }
