6d4e8b2fc85c678d962efc5b3fa032de15e4ee57
junieberry <junyoung44@g.skku.edu>
2021-07-10 23:27:57 +0900

    [feat] exchange activity design

--- app/src/main/java/com/example/gdg11_android/exchange/ExchangeActivity.kt
+++ app/src/main/java/com/example/gdg11_android/exchange/ExchangeActivity.kt
@@ -2,25 +2,61 @@ package com.example.gdg11_android.exchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.gdg11_android.databinding.ActivityExchangeBinding
+import io.socket.client.IO
+import io.socket.client.Socket
+import org.json.JSONObject
+import java.net.URISyntaxException
 
 class ExchangeActivity : AppCompatActivity() {
     lateinit var binding:ActivityExchangeBinding
+    lateinit var mSocket:Socket
+
+    var roo
+    val TAG="로그"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityExchangeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        init()
+
     }
 
+
+
+    fun init(){
+        try {
+            //소켓 서버 연결결
+           mSocket=IO.socket("http://34.146.141.114:3000")
+            mSocket.connect()
+            val data=JSONObject()
+            data.put("room",room_id)
+            mSocket.emit("joinRoomBuyer")
+
+            Log.d(TAG,"Socket connected")
+        }catch (e:URISyntaxException){
+            Log.d(TAG,"Socket connection failed")
+        }
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mSocket.disconnect()
+        Log.d(TAG,"Socket disconnected")
+    }
 
     fun plus_clicked(v:View){
         val plus_view= ExchangeBottomSheet()
         plus_view.show(supportFragmentManager,"plus_view")
     }
     fun o_clicked(v:View){
-        success_dialog()
+        mSocket.emit("requestAccept")
     }
     fun x_clicked(v:View){
         fail_dialog()
@@ -28,7 +64,7 @@ class ExchangeActivity : AppCompatActivity() {
     }
 
     fun success_dialog(){
-        SuccessDialog(context = this).start()
+        SuccessDialog(context = this).show()
     }
 
     fun fail_dialog(){
--- app/src/main/java/com/example/gdg11_android/exchange/SuccessDialog.kt
+++ app/src/main/java/com/example/gdg11_android/exchange/SuccessDialog.kt
@@ -4,25 +4,26 @@ import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.graphics.Point
+import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
 import com.example.gdg11_android.R
 
-class SuccessDialog (context: Context){
+class SuccessDialog(context: Context) : Dialog(context) {
     val dlg=Dialog(context)
 
-    fun start(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dlg.setContentView(R.layout.exchange_success_dialog)
         dlg.setCancelable(false)
-
+        dlg.show()
         dlg.findViewById<Button>(R.id.success_dialog_btn).setOnClickListener {
-            //어쩌구
             dlg.dismiss()
         }
-
-        dlg.show()
     }
+
 }
